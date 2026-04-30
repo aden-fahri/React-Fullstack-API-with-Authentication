65a0539a56a04abebf7e2d13692916c22c52c4b9
aden-fahri <adenathoriq@gmail.com>
2026-04-30 18:30:25 +0700

    membuat API post

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostRequest $request)
+    {
+        $validate = $request->validated();
+
+        $post = Post::create($validate);
+        return $post;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        return $post;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        $validate = $request->validated();
+
+        $post->update($validate);
+        return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return ['message' => 'Post deleted successfully!!'];
+    }
+}
